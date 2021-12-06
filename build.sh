30f8403bfbc4c3469ba0b0b103382d2571e718aa
Jormin <jorminxee@gmail.com>
2021-12-06 18:20:36 +0800

    kafka demo

--- build.sh
+++ build.sh
@@ -4,26 +4,26 @@
 cd gateway
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o gateway main.go
 docker build -t jormin/kafka-demo-gateway:v0.0.1 .
-docker push jormin/kafka-demo-gateway
+docker push jormin/kafka-demo-gateway:v0.0.1
 rm -rf ./gateway
 
 # order
 cd ../order
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o order main.go
 docker build -t jormin/kafka-demo-order:v0.0.1 .
-docker push jormin/kafka-demo-order
+docker push jormin/kafka-demo-order:v0.0.1
 rm -rf ./order
 
 # repository
 cd ../repository
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o repository main.go
 docker build -t jormin/kafka-demo-repository:v0.0.1 .
-docker push jormin/kafka-demo-repository
+docker push jormin/kafka-demo-repository:v0.0.1
 rm -rf ./repository
 
 # statistics
 cd ../statistics
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o statistics main.go
 docker build -t jormin/kafka-demo-statistics:v0.0.1 .
-docker push jormin/kafka-demo-statistics
+docker push jormin/kafka-demo-statistics:v0.0.1
 rm -rf ./statistics
